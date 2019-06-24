d1cdcb9e035eb6e0f3bab1fd17cc43dae5b84844
J1NC <womin1120@naver.com>
2019-06-24 10:27:41 +0900

    Revert "[Modify] Socket.io -> WebSocket"
    This reverts commit 6a78b77f2716f0574fe003625888a6e78d9a9ade.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 let ab2str = require('arraybuffer-to-string');
 
-let ws = new WebSocket('ws://10.80.161.110:3100');
+let socket = io.connect('http://192.168.1.9:3000');
 let base64String = '';
 let canvas = $('#hostScreen')[0];
 let ctx = canvas.getContext("2d");
@@ -11,65 +11,35 @@ image.onload = function() {
 };
 
 window.joinRoom = () => {
-    let req = {event: 'joinRoom', data: $('#hostId').val()}
-    ws.send(JSON.stringify(req));
+    socket.emit('joinRoom', $('#hostId').val());
 }
+socket.on('err', (message) => {
+    alert(message);
+});
 
-ws.onmessage = (response) => {
-    let res = JSON.parse(response.data);
-    let event = res.event;
-    let message = res.message;
+socket.on('suc', (message) => {
+    alert(message);
+    $('#hostId').prop('disabled', true);
+});
 
-    if(event != 'screen' && event != 'headcount')
-        alert(message);
-    switch(event){
-        case 'suc' : 
-            $('#hostId').prop('disabled', true);
-            break;
-        case 'exit' : 
-            location.reload();
-            break;
-        case 'headcount' :
-            $('#headCount').html(message + "명이 현재 접속 중 입니다.");
-            break;
-        case 'screen' :
-            if(message.indexOf('&end') != -1){
-                base64String += message.replace('&end','');
-                image.src = "data:image/png;base64," + base64String;
-                base64String = '';
-            } else {
-                base64String += message;
-            }
-            break;
+socket.on('screen', (screenData) => {
+    let data = '';
+    data = screenData;
+    if(data.indexOf('&end') != -1){
+        base64String += screenData.replace('&end','');
+        image.src = "data:image/png;base64," + base64String;
+        base64String = '';
+    } else {
+        base64String += screenData;
     }
-}
-// socket.on('err', (message) => {
-//     alert(message);
-// });
+})
 
-// socket.on('suc', (message) => {
-//     alert(message);
-//     $('#hostId').prop('disabled', true);
-// });
+socket.on('exit', (message) => {
+    alert(message);
 
-// socket.on('screen', (screenData) => {
-//     let data = '';
-//     data = screenData;
-//     if(data.indexOf('&end') != -1){
-//         base64String += screenData.replace('&end','');
-//         image.src = "data:image/png;base64," + base64String;
-//         base64String = '';
-//     } else {
-//         base64String += screenData;
-//     }
-// })
+    location.reload();
+})
 
-// socket.on('exit', (message) => {
-//     alert(message);
-
-//     location.reload();
-// })
-
-// socket.on('headcount', (data) => {
-//     $('#headCount').html(data + "명이 현재 접속 중 입니다.");
-// })
+socket.on('headcount', (data) => {
+    $('#headCount').html(data + "명이 현재 접속 중 입니다.");
+})
